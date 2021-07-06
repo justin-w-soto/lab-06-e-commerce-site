b38abe60b41650511dc93847a2869cd5210ee8d5
Justin W Soto <soto@pdx.edu>
2021-07-06 16:06:27 -0700

    added some style, still needs work

--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,19 @@
 // IMPORT MODULES under test here:
-// import { example } from '../example.js';
 
+import { renderVisors } from '../render-visors.js';
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
-    //Arrange
-    // Set up your arguments and expectations
-    const expected = true;
+test('does the js insert new li into html in browser', (expect) => {
+    const janetLi = '<li><h3>The Janet</h3><img src="./assets/visor1.png" alt="The Janet"><span>$10</span><button>Add to Cart</button></li>';
     
-    //Act 
-    // Call the function you're testing and set the result to a const
-    const actual = true;
-
-    //Expect
-    // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
+    const v1 = {
+        id: 'visor1',
+        name: 'The Janet',
+        image: 'visor1.png',
+        description: 'A fanny-pack for your face!',
+        category: 'multifunctional',
+        price: 10.00
+    };
+    const actual = renderVisors(v1);
+    expect.equal(actual.outerHTML, janetLi);
 });
